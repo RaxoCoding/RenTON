ab776551b9e47eaaae8261a91c7af87f94388c00
LeTeazer <165527659+LeTeazer@users.noreply.github.com>
2024-10-19 16:23:29 +0200

    Update main.py

--- Telegram/main.py
+++ Telegram/main.py
@@ -4,34 +4,60 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup 
 import time, re
 import sqlite3
-import time
+import time, uuid
+
+###########################################################################################"
 
 DB_PATH = 'user.db'
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-
+   
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS renters (
-        user_id INT,
+        user_id INT NULL,
         uuid TEXT NOT NULL,
         renter_id INT NOT NULL
     )
     ''')
-    cursor.execute("INSERT INTO renters (titre, message, user_agent) VALUES (?, ?, ?)", ())
+    
     conn.commit()
     conn.close()
 
-def format_db():
-    while True:
-        time.sleep(3 * 60) 
-        conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM renters") 
-        conn.commit()
-        conn.close()
-        print("Base de données formatée.")
+
+def insert_in_renters(user_id=None, uid=None, renter_id=None):
+	conn = sqlite3.connect(DB_PATH)
+	cursor = conn.cursor()
+	sql = '''INSERT INTO renters (user_id, uuid, renter_id)
+		VALUES(?,?,?) '''
+	cursor = conn.cursor()
+	cursor.execute(sql, (user_id, uid, renter_id))
+	conn.commit()
+	conn.close()
+
+def check_uuidd(uuid):
+	conn = sqlite3.connect(DB_PATH)
+	cursor = conn.cursor()
+	cursor.execute('''
+	    SELECT renter_id FROM renters WHERE uuid = ?
+	''', (uuid,))
+	record = cursor.fetchone()
+	conn.close()
+	return record[0]
+
+session_manager = {}
+
+init_db()
+str_db1 = str(uuid.uuid4())
+str_db2 = str(uuid.uuid4())
+
+print(str_db1)
+insert_in_renters(None, str_db1, 6324596694)
+insert_in_renters(None, str_db2, 1649278833)
+print(str_db1, str_db2)
+
+###########################################################################################"
 
 # Reste à : 
 # - Identifier la correspondance entre les UUID et le loueur (UUID -> Loueur) ( BDD ) ->
@@ -41,10 +67,10 @@ def format_db():
 # - Se connecter à la blockchain Ton afin de confirmer les transactions etc..
 # - Manager les problèmes.
 # 3ee6d905-4344-4117-a992-1d75a3d99b56
+
 regex_uuid = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
 
-key = "KEYBOT"
-
+key = "7374775362:AAG61pm7UGyWxT8DDcYK6memsH8cfZ6Qwn8"
 
 wlc_msg = """
 Hello,
@@ -83,14 +109,19 @@ Visible on the website page.
 	if message.text == "Accept":
 		# FAIRE EN SORTE QUE SA REFUSE CHEZ LE CLIENT !
 		None
+
 	if message.text == "Offer":
 		message_to_renter = """
 Hey,
 A customer wishes to hire your bike under this ad XXXXX. Do you accept?
 """
-		await bot.send_message(chat_id=, text=message_to_renter, reply_markup=renter_response)
+		await bot.send_message(chat_id=6324596694, text=message_to_renter, reply_markup=renter_response)
 		await message.reply("Offer sent to the renter", reply_markup=keyboard_reply)
+
 	if re.match(regex_uuid, message.text):
-		await message.reply("Select the action you want to take", reply_markup=keyboard_reply_offer)
+		if check_uuidd(message.text) == None:
+			await message.reply("No UUID matches what you have entered. Back to menu", reply_markup=keyboard_reply)
+		else:
+			await message.reply("Select the action you want to take", reply_markup=keyboard_reply_offer)
 	
 executor.start_polling(dp) 
