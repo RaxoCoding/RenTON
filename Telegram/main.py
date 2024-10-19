2e14e7400ae9dcb112d5177c5f1dc1264dcd7ccf
LeTeazer <165527659+LeTeazer@users.noreply.github.com>
2024-10-19 18:45:45 +0200

    Update main.py

--- Telegram/main.py
+++ Telegram/main.py
@@ -1,6 +1,12 @@
+# Reste à : 
+# - Identifier la correspondance entre les UUID et le loueur (UUID -> Loueur) ( BDD ) ->
+# - Ajouter le front à la miniapp afin de pouvoir gérer la conversation dans la miniapp
+# - Ajouter une option de communication manager par le bot, voir comment faire
+# - Se connecter à la blockchain Ton afin de confirmer les transactions etc..
+# - Manager les problèmes.
+# 3ee6d905-4344-4117-a992-1d75a3d99b56
 
-# Importing required libraries 
-from aiogram import Bot, Dispatcher, types, F, executor
+from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import ReplyKeyboardMarkup , KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import re, jwt, uuid
 import sqlite3
@@ -46,6 +52,7 @@ def check_uuidd(uuid):
 	return record[0]
 
 session_manager = {}
+session_tradeoffer = {}
 my_secret = 'my_super_secret'
 init_db()
 str_db1 = str(uuid.uuid4())
@@ -58,14 +65,7 @@ print(str_db1, str_db2)
 
 ###########################################################################################"
 
-# Reste à : 
-# - Identifier la correspondance entre les UUID et le loueur (UUID -> Loueur) ( BDD ) ->
-# - Faire en sorte que le fil soit suivi -> Pouvoir identifier les chatid etc..
-# - Ajouter le front à la miniapp afin de pouvoir gérer la conversation dans la miniapp
-# - Ajouter une option de communication manager par le bot, voir comment faire
-# - Se connecter à la blockchain Ton afin de confirmer les transactions etc..
-# - Manager les problèmes.
-# 3ee6d905-4344-4117-a992-1d75a3d99b56
+
 
 regex_uuid = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
 
@@ -79,7 +79,7 @@ What action do you want to take?
 
 bot = Bot(token=key) 
 
-dp = Dispatcher() 
+dp = Dispatcher(bot) 
 
 menu = InlineKeyboardMarkup(row_width=1, inline_keyboard=[[
 	InlineKeyboardButton(text="Hire", callback_data="Hire_bike"),
@@ -92,30 +92,63 @@ do_offer = InlineKeyboardMarkup(row_width=1, inline_keyboard=[[
 	InlineKeyboardButton(text="Cancel", callback_data="Cancel"),
 	]])
 
-accept_deny = InlineKeyboardMarkup(row_width=1, inline_keyboard=[[
-	InlineKeyboardButton(text="Accept", callback_data="Accept"),
-	InlineKeyboardButton(text="Deny", callback_data="Deny"),
-	]])
 
-dp.callback_query_handlers.register(callback_data.ok_action, F.data == 'Hire_bike')
 
+
+@dp.callback_query_handler(text = "Hire_bike")
+async def hire(call: types.CallbackQuery):
+	returned_msg = """
+Enter the location ID. 
+Visible on the website page.
+"""		
+	await call.message.answer(text=returned_msg)
+
+
+
+@dp.callback_query_handler(text = "Offer")
+async def offer(call: types.CallbackQuery):
+
+	message = call.from_user["id"]
+	infos = jwt.decode(session_manager[message], key=my_secret, algorithms=['HS256'])
+	message_to_renter = f"""
+Hey,
+A customer wishes to hire your bike under this ad {infos["uuid"]}. Do you accept?
+"""
+	
+	uuid_offer = uuid.uuid4()
+	payload = {
+		"client_id":message,
+		"renter_id":infos["renter_id"],
+		"uuid":str(uuid_offer)
+	}
+	session_tradeoffer[uuid_offer] = jwt.encode(payload=payload, key=my_secret)
+
+	accept_deny = InlineKeyboardMarkup(row_width=1, inline_keyboard=[[
+		InlineKeyboardButton(text="Accept", callback_data=f"{uuid_offer}-oui"),
+		InlineKeyboardButton(text="Deny", callback_data=f"{uuid_offer}-non")
+		]])
+
+	await bot.send_message(text=message_to_renter, chat_id=infos["renter_id"], reply_markup=accept_deny)
+	await call.message.edit_text(text="Offer sent to the renter", reply_markup=menu)
+
+
+@dp.callback_query_handler()
+async def accept_deny(call: types.CallbackQuery):
+	call_uuid = call.data[:-4]
+	data = jwt.decode(session_tradeoffer[call_uuid], key=my_secret, algorithms=["HS256"]) ## CRASH
+	if call_uuid+"-oui" == call.data:
+		message = "Oui"
+		await bot.send_message(text = message, chat_id=data["client_id"])
+	elif call_uuid+"-non" == call.data:
+		message = "Non"
+		await bot.send_message(text = message, chat_id=data["client_id"])
+	
 @dp.message_handler(commands=['start', 'help']) 
 async def welcome(message: types.Message): 
 	await message.reply(wlc_msg, reply_markup=menu) 
 
 @dp.message_handler() 
 async def check_rp(message: types.Message): 
-
-	if message.text == "Hire a bike": 
-		returned_msg = """
-Enter the location ID. 
-Visible on the website page.
-"""		
-		await message.reply(returned_msg)
-
-	if message.text == "Accept":
-		# FAIRE EN SORTE QUE SA REFUSE CHEZ LE CLIENT !
-		None
 
 	if message.text == "Offer":
 
