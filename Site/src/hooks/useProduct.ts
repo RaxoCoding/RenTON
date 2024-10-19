48f8c46737bcb8fbedaee0baf26d4b7a2a11e377
LighTender - Dev <87587438+LighTend3r@users.noreply.github.com>
2024-10-19 15:01:22 +0200

    comment unsed parties

--- Site/src/hooks/useProduct.ts
+++ Site/src/hooks/useProduct.ts
@@ -4,23 +4,23 @@ import { FullProduct } from "@/types/product";
 
 
 
-const productMock = {
-	id: '123',
-	name: "Mountain Explorer Pro",
-	images: ["/bike.jpg", "/bike.jpg"],
-	pricePerHour: 15,
-	cautionPrice: 250,
-	owner: {
-		id: "owner123",
-		username: "bikeEnthusiast",
-		avatar: "/placeholder.svg?height=50&width=50",
-		rating: 4.7,
-		telegramHandle: "@bikeEnthusiast",
-		walletAddress: "0:123",
-	},
-	description:
-		"A high-performance mountain bike perfect for rough terrains and adventurous trails. Features 21-speed gears, front suspension, and durable tires.",
-};
+// const productMock = {
+// 	id: '123',
+// 	name: "Mountain Explorer Pro",
+// 	images: ["/bike.jpg", "/bike.jpg"],
+// 	pricePerHour: 15,
+// 	cautionPrice: 250,
+// 	owner: {
+// 		id: "owner123",
+// 		username: "bikeEnthusiast",
+// 		avatar: "/placeholder.svg?height=50&width=50",
+// 		rating: 4.7,
+// 		telegramHandle: "@bikeEnthusiast",
+// 		walletAddress: "0:123",
+// 	},
+// 	description:
+// 		"A high-performance mountain bike perfect for rough terrains and adventurous trails. Features 21-speed gears, front suspension, and durable tires.",
+// };
 
 export function useProduct(productId: string) {
   // const fetchProduct = async (): Promise<FullProduct | null> => {
